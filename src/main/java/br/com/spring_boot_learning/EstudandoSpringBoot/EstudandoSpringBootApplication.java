53ae896e5eee5d1a275a9fa82698b133e07d26aa
Pedro Pereira <devpedrohp@hotmail.com>
2026-04-20 19:11:43 -0300

    first commit

---
+++ src/main/java/br/com/spring_boot_learning/EstudandoSpringBoot/EstudandoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring_boot_learning.EstudandoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spring_boot_learning/EstudandoSpringBoot/EstudandoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring_boot_learning.EstudandoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
